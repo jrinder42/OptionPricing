92abbcd9f0466baca58796894dc7857658715d5f
Jordan Rinder <Jordan@0587394520.wireless.umich.net>
2016-09-16 19:58:33 -0400

    Adding Option Pricing Folder

--- OptionPricing/European.py
+++ OptionPricing/European.py
@@ -1,4 +1,3 @@
-import math
 import numpy as np
 from random import gauss
 from scipy.stats import norm
@@ -28,18 +27,18 @@ class European:
 	def MonteCarlo(self, option):
 		if option.lower() == "call":
 			simulations = 50000
-			disc_factor = math.exp(-self.r * self.T)
+			disc_factor = np.exp(-self.r * self.T)
 			payoffs = []
 			for i in xrange(simulations):
-				S_t = self.S * math.exp((self.r - 0.5 * self.sigma**2) * self.T + self.sigma * math.sqrt(self.T) * gauss(0,1.0))
+				S_t = self.S * np.exp((self.r - 0.5 * self.sigma**2) * self.T + self.sigma * np.sqrt(self.T) * gauss(0,1.0))
 				payoffs.append(max(0.0, S_t - self.X))
 			return disc_factor * (sum(payoffs) / float(simulations))
 		elif option.lower() == "put":
 			simulations = 50000
-			disc_factor = math.exp(-self.r * self.T)
+			disc_factor = np.exp(-self.r * self.T)
 			payoffs = []
 			for i in xrange(simulations):
-				S_t = self.S * math.exp((self.r - 0.5 * self.sigma**2) * self.T + self.sigma * math.sqrt(self.T) * gauss(0,1.0))
+				S_t = self.S * np.exp((self.r - 0.5 * self.sigma**2) * self.T + self.sigma * np.sqrt(self.T) * gauss(0,1.0))
 				payoffs.append(max(0.0,self.X - S_t))
 			return disc_factor * (sum(payoffs) / float(simulations))
 		else:
@@ -47,23 +46,24 @@ class European:
 
 
 	def BlackScholes(self, option):
-		d1 = ((self.S/self.X) + (self.r-self.d + ((self.sigma**2)/2))) / (self.sigma*math.sqrt(self.T))
-		d2 = d1 - self.sigma*math.sqrt(self.T)
+		d1 = (np.log(self.S/self.X) + ((self.r-self.d) + ((self.sigma**2)*0.5))*self.T) / (self.sigma*np.sqrt(self.T))
+		d2 = d1 - self.sigma*np.sqrt(self.T)
 		if option.lower() == "call":
-			return self.S * math.exp(-self.d*self.T) * norm.cdf(d1) - self.X * math.exp(-self.r*self.T) * norm.cdf(d2)
+			return self.S * np.exp(-self.d*self.T) * norm.cdf(d1) - self.X * np.exp(-self.r*self.T) * norm.cdf(d2)
 		elif option.lower() == "put":
-			return self.X * math.exp(-self.r*self.T) * norm.cdf(-d2) - self.S * math.exp(-self.d*self.T) * norm.cdf(-d1)
+			return self.X * np.exp(-self.r*self.T) * norm.cdf(-d2) - self.S * np.exp(-self.d*self.T) * norm.cdf(-d1)
 		else:
 			return "Invalid data"
-		#return price
 
 	# Cox-Ross-Rubenstein Binomial Pricing
 	def CRR(self, option, steps):
-		dt = self.T/steps
+		self.T = float(self.T)
+		dt = round(self.T/steps, 5)
 		v = self.r - self.d
 		up = np.exp(v * np.sqrt(dt))
 		down = 1./up
 		p = (np.exp(self.r * dt) - down)/(up - down)
+
 
 		# Binomial Price Tree
 		val = np.zeros((steps + 1, steps + 1))
@@ -87,3 +87,5 @@ class European:
 				price[i, j] = np.exp(-self.r*dt)*(p*price[i+1,j] + (1-p)*price[i+1,j+1])
 
 		return price[0, 0]
+
+
